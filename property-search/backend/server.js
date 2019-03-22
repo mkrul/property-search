a812435662857f89317be6ae7a19579667a4e3f3
mkrul <mishakrul@gmail.com>
2019-03-22 08:27:08 -0400

    back end is connecting to mongodb

--- property-search/backend/server.js
+++ property-search/backend/server.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
+const fs = require("fs");
 
 const app = express();
 const PORT = 4000;
+const password = fs.readFileSync("password.txt", "utf8");
+const uri = `mongodb+srv://mkrul:${password}@cluster0-uadok.mongodb.net/test?retryWrites=true`; 
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect();
+const client = MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  console.log("MongoDB connection established!");
+  client.close();
+});
 
 app.listen(PORT, function() {
   console.log(`Server is running on port ${PORT}`);
