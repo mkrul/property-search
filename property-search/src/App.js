8c0b5ca3db33c3bd834a2f4aaa5b699b5ea36eb7
mkrul <mishakrul@gmail.com>
2019-03-22 05:48:33 -0400

    add dependencies

---
+++ property-search/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>Property Search</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
